0420165001b8dd5fb5af922c7460c7264d90d608
Joy Ngera <wangjoy004@gmail.com>
2025-04-13 01:11:29 +0300

    Initialize Heroku deployment commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include '../includes/header.php'; ?>
+<?php include '../includes/navbar.php'; ?>
+
+<h1>Welcome to Agrisphere</h1>
+<p>Your platform for farm produce sales</p>
+
+<?php include '../includes/footer.php'; ?>
